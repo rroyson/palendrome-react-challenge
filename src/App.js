44ef4ce0a8991f0d40f84d9aeb3e719bd94ef767
Rob Royson <rroyson@Robs-MBP.attlocal.net>
2022-06-09 15:15:47 -0400

    Add palandrome code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
+  const { useState } = require('react')
+
+  const [name, setName] = useState('')
+  const [isPalendrome, setPalendrome] = useState(null)
+
+  const handleChange = (e) => {
+    setName(e.target.value)
+  }
+
+  const checkPalanedrome = (word) => {
+    if (word === '') return false
+    const reversed = word.split('').reverse().join('')
+
+    if (word === reversed) return true
+
+    return false
+  }
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    const checkP = checkPalanedrome(name)
+    return checkP ? setPalendrome(true) : setPalendrome(false)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Check if a word is a palandrome</h1>
+      <form>
+        <input
+          onChange={handleChange}
+          value={name}
+          placeholder='Enter a word to check'
+        ></input>
+        <button onClick={handleClick}>Submit</button>
+      </form>
+      {isPalendrome === null ? (
+        <div></div>
+      ) : isPalendrome ? (
+        <h3>This is a palandrome</h3>
+      ) : (
+        <h3>Not a palandrome</h3>
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
